9c6f6baf562706cb69b18ecc8be7868b403eeb32
Ward9250 <ben.ward@sainsbury-laboratory.ac.uk>
2013-09-14 09:27:25 +0100

    Fixed the NaN trouble with mosaic plots with too High MosaicScale options on data with few informative sites / smaller sequences

--- R/MosaicPlot.R
+++ R/MosaicPlot.R
@@ -14,6 +14,14 @@ vertbar_create <- function( ssobj, plotframerow, whichcomp )
 #' @export
 col_deter <- function( invalues, reference ) 
 {
-  cols <- reference[reference[, 1] == as.numeric( invalues[1] ) & reference[,2] == as.numeric( invalues[2] ), 3]
+  if(any( is.nan( as.numeric(invalues) ) ) ){
+    warning("\nNot a numbers (NaNs)! have been detected in the plotting frame.\n
+The common cause of this is a small alignment or few informative sites in the data, 
+with a too high MosaicScale parameter.\nThis usually happens at either end of the 
+bars and the NaNs will be dealt with my filling them in black.\n\nTo get rid of them use a lower MosaicScale parameter.")
+    cols <- "#000000"
+  } else {
+    cols <- reference[reference[, 1] == as.numeric( invalues[1] ) & reference[,2] == as.numeric( invalues[2] ), 3]
+  }
   return( cols )
 }
