b49ce171c7e9ae1cd8f2dc313d134a10b916a189
ward9250 <ben.ward@sainsbury-laboratory.ac.uk>
2014-02-14 09:00:23 +0000

    change

--- R/BlockDateInternals.R
+++ R/BlockDateInternals.R
@@ -26,6 +26,61 @@ comb <- function(B,D){
   return(B)
 }
 
+# #########################################################################################################################
+# 
+mutation.correction <- function(ObservedMutations, SequenceSize){
+  ObservedRatio <- ObservedMutations/SequenceSize
+  ActualRatio <- seq(from=0, to=2, by=0.0001)
+  OutputRatio <- (0.992582633 * ActualRatio) - (0.605566567 * (ActualRatio^2) ) + (0.166571989 * (ActualRatio^3) )
+  ResultRatio <- ActualRatio[which(OutputRatio >= ObservedRatio)[1]]
+  #Output <- ceiling(ResultRatio * SequenceSize)
+  return(ResultRatio)
+}
+# 
+# ActualRatio <- seq(from=0, to=1, by=0.0001)
+# OutputRatio <- (0.9980 * ActualRatio) - (0.6170 * (ActualRatio^2) ) + (0.1704 * (ActualRatio^3) )
+# ResultRatio <- ActualRatio[which(OutputRatio >= ObservedRatio)[1]]
+# 
+# OutputRatio <- (0.992582633 * ActualRatio) - (0.605566567 * (ActualRatio^2) ) + (0.166571989 * (ActualRatio^3) )
+# 
+# 
+# results_10000_03$testresult[which(is.na(results_10000_03$testresult))] <- apply(results_1000_04[which(is.na(results_10000_03$testresult)),], 1, function(x) mutation.correction(x[1], x[4]))
+# 
+# lm(results_1000_03$ObservedSNPsRatio ~ results_1000_03$ActualSNPsRatio + I(results_1000_03$ActualSNPsRatio^2) + I(results_1000_03$ActualSNPsRatio^3))
+# 
+# ActualRatio <- seq(from=0, to=2, by=0.0001)
+# OutputRatio <- (0.992582633 * ActualRatio) - (0.605566567 * (ActualRatio^2) ) + (0.166571989 * (ActualRatio^3) )
+# 
+# model <- lm(results_10000_03$ActualSNPsRatio ~ results_1000_03$ObservedSNPsRatio + I(results_1000_03$ObservedSNPsRatio^2) + I(results_1000_03$ObservedSNPsRatio^3))
+# x <- seq(from=0, to=0.7, by=0.1)
+# plot(results_10000_03$ObservedSNPsRatio, results_10000_03$ActualSNPsRatio)
+# points(predict(model), col="red")
+# 
+# results_5000_02$testresult <- apply(results_5000_02, 1, function(x) mutation.correction(x[1], x[4]))
+# results_100_02$testresult <- apply(results_100_02, 1, function(x) mutation.correction(x[1], x[4]))
+# results_1000_03$testresult <- apply(results_1000_03, 1, function(x) mutation.correction(x[1], x[4]))
+# results_10000_03$testresult <- apply(results_10000_03, 1, function(x) mutation.correction(x[1], x[4]))
+# results_100_03$testresult <- apply(results_100_03, 1, function(x) mutation.correction(x[1], x[4]))
+# results_5000_03$testresult <- apply(results_5000_03, 1, function(x) mutation.correction(x[1], x[4]))
+# results_10000_04$testresult <- apply(results_10000_04, 1, function(x) mutation.correction(x[1], x[4]))
+# results_1000_04$testresult <- apply(results_1000_04, 1, function(x) mutation.correction(x[1], x[4]))  
+# results_5000_04$testresult <- apply(results_5000_04, 1, function(x) mutation.correction(x[1], x[4]))
+# 
+# save(results_1000_03,file="results_1000_03_highreps")
+# save(results_1000_03,file="results_1000_03_highreps")
+# save(results_10000_03,file="results_10000_03_highreps")
+# save(results_1000_04,file="results_1000_04_highreps")
+# save(results_10000_04,file="results_10000_04_highreps")
+# save(results_10000_05,file="results_10000_05_highreps")
+# save(results_100_03,file="results_100_03_highreps")
+# save(results_100_02,file="results_100_02_highreps")
+# save(results_5000_04,file="results_5000_04_highreps")
+# save(results_5000_03,file="results_5000_03_highreps")
+# save(results_5000_02,file="results_5000_02_highreps")
+# 
+# ##########################################################################################################
+
+
 binomcalc <- function(p, p0, N, B){pbinom(B,N,p)-p0}
 
 date.blocks <- function(blocksobj, dnaobj, mut, pair, pthresh, bonfcorrect, danyway) {
@@ -52,6 +107,7 @@ date.blocks <- function(blocksobj, dnaobj, mut, pair, pthresh, bonfcorrect, dany
       maxSNPs <- dist.dna(as.DNAbin(Seq), model="N")[1]
       # Probability of observing this many SNPs in the block given the divergence of the whole sequence divergence of the sequences. 
       pValue <- pbinom( maxSNPs, N, wholeSequenceDist )
+      #maxSNPs <- mutation.correction(maxSNPs, N)
       if( pValue > pthresh && danyway == FALSE ) {
         next # If the block does not meet the p-value threshold, drop it and proceed to next loop iteration.
       } else {
@@ -61,12 +117,9 @@ date.blocks <- function(blocksobj, dnaobj, mut, pair, pthresh, bonfcorrect, dany
         soln5 <- uniroot(binomcalc, c(0,1), p0=0.05, B=maxSNPs, N=N)
         soln50 <- uniroot(binomcalc, c(0,1), p0=0.5, B=maxSNPs, N=N)
         soln95 <- uniroot(binomcalc, c(0,1), p0=0.95, B=maxSNPs, N=N)
-	      corrected5 <- (3-(9-(12*soln5[["root"]]))^0.5)*0.5
-	      corrected50 <- (3-(9-(12*soln50[["root"]]))^0.5)*0.5
-	      corrected95 <- (3-(9-(12*soln95[["root"]]))^0.5)*0.5
-        blockAges[i,3] <- round(corrected5/(2*mut))
-        blockAges[i,2] <- round(corrected50/(2*mut))
-        blockAges[i,1] <- round(corrected95/(2*mut))
+        blockAges[i,3] <- round(soln5[["root"]]/(2*mut))
+        blockAges[i,2] <- round(soln50[["root"]]/(2*mut))
+        blockAges[i,1] <- round(soln95[["root"]]/(2*mut))
         blockAges[,7] <- pthresh
       }
     }
