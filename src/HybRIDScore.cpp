9ab2f0969f3eb9e9b1aae9cc288575fd963f01c2
Ward9250 <axolotlfan9250@gmail.com>
2014-09-16 17:07:27 +0100

    Bit more work on C++ scan

--- src/HybRIDScore.cpp
+++ src/HybRIDScore.cpp
@@ -1,12 +1,6 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
-// Below is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using the Rcpp::sourceCpp 
-// function (or via the Source button on the editor toolbar)
-
-// For more on using Rcpp click the Help button on the editor toolbar
-
 // [[Rcpp::export]]
 LogicalVector sequenceChecker_cpp(CharacterMatrix x){
   int nc = x.ncol() ;
@@ -21,13 +15,16 @@ LogicalVector sequenceChecker_cpp(CharacterMatrix x){
   return out;
 }
 
-DataFrame ScanLoop_cpp(DataFrame df){
+DataFrame ScanLoop_cpp(DataFrame df, CharacterMatrix seq){
+  std::vector<int> frompos = Rcpp::as< std::vector<int> >(df["WindowStart"]);
+  std::vector<int> topos = Rcpp::as< std::vector<int> >(df["WindowEnd"]);
   std::vector<double> AB = Rcpp::as< std::vector<double> >(df["AB"]);
   std::vector<double> AC = Rcpp::as< std::vector<double> >(df["AC"]);
   std::vector<double> BC = Rcpp::as< std::vector<double> >(df["BC"]);
   
-  for (int i = 0; i < AB.size() ; i++){
-    v_prob[i] = vaccinate_cxx(age_v[i],female_v[i],ily_v[i]); 
+  for (int i = 0; i < AB.size(); i++){
+    CharacterMatrix dnaStretch = seq(_, Range(frompos[i], topos[i]));
+    
   }
   
 }
