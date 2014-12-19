3080387d7c9ee9d8b636415bdce7985a1906836c
Ward9250 <axolotlfan9250@gmail.com>
2014-12-19 21:16:25 +0000

    Added unit test for object that stores sequence similarity tables

---
+++ tests/testthat/testTriplets.R
@@ -0,0 +1,23 @@
+context("tesing functionality of triplets.")
+
+test_that("reference objects storing sequence similarity scan tables work correctly", {
+  data(MySequences)
+  hyb <- HybRIDS$new(MySequences)
+  ssObj <- hyb$triplets$triplets[[1]]$ScanData
+  expect_is(ssObj$TableFile, "character")
+  expect_is(ssObj$Table, "data.frame")
+  expect_true(ssObj$tableIsBlank())
+  hyb$analyzeSS(c("Seq1", "Seq2", "Seq3"))
+  expect_false(ssObj$tableIsBlank())
+  expect_true(!any(apply(ssObj$Table, 2, function(x) any(is.na(x)))))
+  ssObj$blankTable()
+  expect_true(ssObj$tableIsBlank())
+})
+
+
+
+
+
+
+
+
