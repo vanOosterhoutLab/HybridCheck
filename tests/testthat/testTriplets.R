dae8255b047087184dd24aa8dd935e5d26b2a7dc
Ward9250 <axolotlfan9250@gmail.com>
2015-02-27 16:37:49 +0000

    Fixed test of SS analysis tables

--- tests/testthat/testTriplets.R
+++ tests/testthat/testTriplets.R
@@ -7,8 +7,6 @@ test_that("reference objects storing sequence similarity scan tables work correc
   ssObj <- hyb$triplets$triplets[[1]]$ScanData
   expect_is(ssObj$TableFile, "character")
   expect_is(ssObj$Table, "data.frame")
-  expect_true(ssObj$tableIsBlank())
-  hyb$analyzeSS(c("Seq1", "Seq2", "Seq3"))
   expect_false(ssObj$tableIsBlank())
   expect_true(!any(apply(ssObj$Table, 2, function(x) any(is.na(x)))))
   ssObj$blankTable()
